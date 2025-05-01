67abc1ea420f03e418485505f121b63e1b068f0f
simon <sbmp2000@gmail.com>
2025-04-30 19:48:39 -0700

    added taking the best of 10 trials

--- gd.py
+++ gd.py
@@ -16,10 +16,19 @@ def gradient_descent(eta, f, iters):
   for i in range(iters):
     x -= eta * dx(x)
     y -= eta * dy(y)
-  print(f" Gradient descent with step size {eta}: starting point ({x_init}, {y_init}), final min f({x:.3f}, {y:.3f}) = {func(x, y):.3f}")
+  return x, y, x_init, y_init
     
 
 
 max_iters = 500
 for step in [0.1, 0.01, 0.001]:
-  gradient_descent(step, func, max_iters)
+  min_val = min_x = min_y = x_init = y_init = 99999
+  for i in range(10): #report the best of 10 trials
+    x, y, x_0, y_0 = gradient_descent(step, func, max_iters)
+    if func(x, y) < min_val:
+      min_val = func(x, y)
+      min_x = x
+      min_y = y
+      x_init = x_0
+      y_init = y_0
+  print(f"Step size {step}, best of 10 trials: starting point ({x_init}, {y_init}), final min f({min_x:.3f}, {min_y:.3f}) = {min_val:.3f}")
