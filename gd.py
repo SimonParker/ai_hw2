311b53a23dd224676489cb54f57fea643fab2b06
simon <sbmp2000@gmail.com>
2025-04-30 19:37:35 -0700

    done

--- gd.py
+++ gd.py
@@ -1,29 +1,25 @@
 #simon parker
 import random
 
-
-
-step = 0.1
-
 def func(x, y):
   return 5*pow(x ,2) + 40*x + pow(y, 2) - 12*y + 127
 
+def dx(x):
+  return 10*x + 40
 
-def GD(eta, f):
-  pass
+def dy(y):
+  return 2*y - 12
 
 def run_tests(eta, f, iters):
   x = x_init = random.randint(-10, 10)
   y = y_init = random.randint(-10, 10)
   for i in range(iters):
-   #x = gradient descent on x
-   #y = gradient descent on y
-   pass
-  print(f" Gradient descent with step size {eta}: starting point ({x_init}, {y_init}), final min ({x}, {y})")
+    x -= eta * dx(x)
+    y -= eta * dy(y)
+  print(f" Gradient descent with step size {eta}: starting point ({x_init}, {y_init}), final min f({x:.3f}, {y:.3f}) = {func(x, y):.3f}")
     
 
 
 max_iters = 500
-for i in range(3):
+for step in [0.1, 0.01, 0.001]:
   run_tests(step, func, max_iters)
-  step *= 0.1
